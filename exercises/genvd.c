668751db745a7dd8cc11f45c1bf7addf115ed07a
sammyovia <sammyigbinovia@gmail.com>
2022-08-29 22:05:33 +0000

    adding gbv

--- exercises/genvd.c
+++ exercises/genvd.c
@@ -5,10 +5,10 @@
  *
  * Return: Always 0.
  */
-int main(void)
+void main(void)
 {
-	extern char **environ;
-	printf("The begininng of environ is %p\n",&(*environ));
+	extern char **environ;       
+        printf("The begininng of environ is %p\n",&(*environ));
 
-    return (0);
+   /* return (0);*/
 }
