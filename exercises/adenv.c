98f392b65ec481304dbed83b5b579cef9b4581ee
Osarieme Naomi Osunde <naomiosariemeosunde@gmail.com>
2022-08-29 14:43:13 -0700

    test

---
+++ exercises/adenv.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the environment
+ *
+ * Return: Always 0.
+ */
+void main(int argc, char *argv[], char *env[])
+{
+        
+        printf("The begininng of env is %p\n",&(*env));
+
+   /* return (0);*/
+}
